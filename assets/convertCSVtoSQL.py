f54a6dbe6c4ccd135eb22ad9c6817964e559336e
Esther <estherzeng@EstherdeMacBook-Pro.local>
2024-06-18 00:19:01 -0700

    Modified the function to change the insert statement format.

--- assets/convertCSVtoSQL.py
+++ assets/convertCSVtoSQL.py
@@ -1,28 +1,26 @@
 import csv
 import os
 
-
-def generate_insert_statements(csv_file, table_name):
+def generate_insert_statement(csv_file, table_name):
     with open(csv_file, mode='r', encoding='utf-8-sig') as file:
         reader = csv.reader(file)
-        headers = next(reader)
-        insert_statements = []
+        next(reader)  # Skip the header row
+        values_list = []
         for row in reader:
             values = ', '.join([f"'{value}'" if value else 'NULL' for value in row])
-            headers_string = ', '.join(headers)
-            insert_statement = f"INSERT INTO {table_name} ({headers_string}) VALUES ({values});"
-            insert_statements.append(insert_statement)
-    return insert_statements
+            values_list.append(f"({values})")
+        values_string = ',\n'.join(values_list)
+        insert_statement = f"INSERT INTO {table_name} VALUES\n{values_string};"
+    return insert_statement
 
 
 def main():
     current_dir = os.path.dirname(__file__)  # Get the directory of the current script
-    csv_file = os.path.join(current_dir, 'mockdata', 'rating.csv')
-    table_name = 'rating'
-    insert_statements = generate_insert_statements(csv_file, table_name)
+    csv_file = os.path.join(current_dir, 'mockdata', 'species_breed.csv')
+    table_name = 'species_breed'
+    insert_statement = generate_insert_statement(csv_file, table_name)
     with open(f'{table_name}.sql', mode='w') as file:
-        file.write('\n'.join(insert_statements))
-
+        file.write(insert_statement)
 
 if __name__ == '__main__':
     main()
